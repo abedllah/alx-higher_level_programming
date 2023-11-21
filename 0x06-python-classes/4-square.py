39b8e4ae9d1b97ba36a2c045e01fc5703afb8f28
Abdellah <abdellahtaghouni@gmail.com>
2023-11-21 15:22:13 -0800

    tsak4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,23 +5,25 @@
 
 class Square:
     """the body of the class """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initializes a new Square """
         self.__size = size
-
-    @property
-    def size(self):
-        """size gitter"""
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
 
      def area(self):
         """Public instance method"""
         return (self.__size ** 2)
+
+     @property
+    def size(self):
+        """Setter for square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Initializes attribute size """
+        if (type(value) is not int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
