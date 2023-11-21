609aef25b33f6fe886cdc4feb8997c4633b32c49
Abdellah <abdellahtaghouni@gmail.com>
2023-11-21 15:13:32 -0800

    task3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""creating class square """
+
+
+
+class Square:
+    """the body of the class """
+    def __init__(self, size=0):
+        """Initializes a new Square """
+        if(type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Public instance method"""
+        return (self.__size ** 2)
