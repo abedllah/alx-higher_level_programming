e169b07d34ccd117cbac328eb31978294d205b64
Abdellah <abdellahtaghouni@gmail.com>
2023-11-21 14:58:20 -0800

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Filename:  0-square"""
+
+
+class Square:
+    """An empty class Square that defines a square:"""
+    def __init__(self,size):
+    """Initializes private attribute size """
+        self.__size=size
