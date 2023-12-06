94b4010285a9f9377b0e8dce01434967f7145213
Abdellah <abdellahtaghouni@gmail.com>
2023-12-06 14:48:41 -0800

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""write_file: write_file()"""
+
 def number_of_lines(filename=""):
+    """my function"""
     cont = 0
     with open(filename, 'r') as f:
         for li in f:
