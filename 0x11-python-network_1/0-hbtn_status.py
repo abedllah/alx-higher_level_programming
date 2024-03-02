c933d73632656b11720c6435396c6fca230cca4a
abdellah <abdellahtaghouni@gmail.com>
2024-03-02 13:51:23 -0800

    task0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""fetch"""
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+    utf8_content = content.decode('utf-8')
+
+print("Body response:")
+print("    - type:", type(content))
+print("    - content:", content)
+print("    - utf8 content:", utf8_content)
+
