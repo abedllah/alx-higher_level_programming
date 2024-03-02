6a18c9c9017d318a5197d2317b11b10ca8bb5a09
abdellah <abdellahtaghouni@gmail.com>
2024-03-02 14:30:36 -0800

    task4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module using request that fetches https://intranet.hbtn.io/status
+"""
+import requests
+
+def main():
+    url = 'https://intranet.hbtn.io/status'
+    r = requests.get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(r.text)))
+    print('\t- content: {}'.format(r.text))
+
+if __name__ == "__main__":
+    main()
